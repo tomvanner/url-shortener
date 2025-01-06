70b105322798d3d2fc8cb733f464aee270ecfcbd
tomvanner <thomasvanner98@gmail.com>
2025-01-06 20:03:54 +0000

    Replace in memory store with sqlite

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomvanner/url-shortener
 
 go 1.23.1
+
+require github.com/mattn/go-sqlite3 v1.14.24 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
-	"sync"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	urlStore = make(map[string]string)
-	mu       sync.Mutex
+	db *sql.DB
 )
 
 type CreateData struct {
 	LongURL string `json:"longURL"`
+}
+
+func initDB() {
+	var err error
+	db, err = sql.Open("sqlite3", "./urls.db")
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping the database: %v", err)
+	}
+
+	fmt.Println("Database connection established")
 }
 
 func createShortURLHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,27 +45,35 @@ func createShortURLHandler(w http.ResponseWriter, r *http.Request) {
 	var d CreateData
 	err := json.NewDecoder(r.Body).Decode(&d)
 
-	// @todo: add url validation
 	if err != nil || !isValidURL(d.LongURL) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	shortURL := generateShortURL()
+	slug := generateSlug()
 
-	mu.Lock()
-	urlStore[shortURL] = d.LongURL
-	mu.Unlock()
+	// @todo: check if slug already exists
+	_, err = db.Exec(
+		"INSERT INTO urls (long_url, slug, created) VALUES ($1, $2, $3)",
+		d.LongURL,
+		slug,
+		time.Now().Format(time.RFC3339),
+	)
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Failed to create short URL", http.StatusInternalServerError)
+		return
+	}
 
 	response := map[string]string{
-		"short_url": "http://localhost:8000/" + shortURL,
+		"short_url": "http://localhost:8000/" + slug,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
-func generateShortURL() string {
+func generateSlug() string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	const length = 6
 
@@ -62,14 +89,17 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 
-	shortURL := r.URL.Path[1:]
+	slug := r.URL.Path[1:]
 
-	mu.Lock()
-	longURL, exists := urlStore[shortURL]
-	mu.Unlock()
+	var longURL string
+	err := db.QueryRow("SELECT long_url FROM urls WHERE slug = $1", slug).Scan(&longURL)
 
-	if !exists {
-		http.Error(w, "Short URL not found", http.StatusNotFound)
+	if err == sql.ErrNoRows {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		log.Fatal(err)
+		http.Error(w, "There was an error. Please try again later.", http.StatusInternalServerError)
 		return
 	}
 
@@ -82,6 +112,9 @@ func isValidURL(urlString string) bool {
 }
 
 func main() {
+	initDB()
+	defer db.Close()
+
 	http.HandleFunc("/", redirectHandler)
 	http.HandleFunc("/create", createShortURLHandler)
 
